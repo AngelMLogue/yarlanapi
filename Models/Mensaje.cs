184fabbc5a2a7adbf0b7ad5a02caa46e9a714eae
angelMLogue <skate8394@gmail.com>
2023-11-17 11:25:56 -0600

    first commit

---
+++ Models/Mensaje.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YarlanAPI.Models;
+
+public partial class Mensaje
+{
+    public int Id { get; set; }
+
+    public string? Alerta { get; set; }
+
+    public string? TipoUsuario { get; set; }
+
+    public string? Zona { get; set; }
+
+    public DateTime? FechaCreacion { get; set; }
+}
---
+++ Models/YarlanDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace YarlanAPI.Models;
+
+public partial class YarlanDbContext : DbContext
+{
+
+    public YarlanDbContext(DbContextOptions<YarlanDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Mensaje> Mensajes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Mensaje>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Mensajes__3214EC27A21BE96A");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Alerta).HasColumnName("Mensaje");
+            entity.Property(e => e.TipoUsuario).HasColumnName("TipoUsuario");
+            entity.Property(e => e.Zona).HasColumnName("Zona");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
